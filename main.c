9567728c11c0f1e0ae0fbcc98bf7b30c5e7c2e9e
Fontes <fontesjpa@gmail.com>
2023-11-27 21:14:05 +0000

    new file:   main.c 	deleted:    main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//VARIAVEIS GLOBAIS
+char login_p1[] = "port1";
+char senha_p1[] = "port1";
+
+char login_p2[] = "port2";
+char senha_p2[] = "port2";
+
+char login_p3[] = "port3";
+char senha_p3[] = "port3";
+
+char login_p4[] = "port4";
+char senha_p4[] = "port4";
+
+void menu(){
+    printf("BEM VINDO AOS PÓRTICOS DA A28");
+
+xcxc
+}
+
+void main(){
+    menu();
+}
